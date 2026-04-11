304f761245d37566b088fffbc13d1d0248c2a653
pabodhainduwari <pabodhainduwari@gmail.com>
2026-04-12 00:36:48 +0530

    feat(booking): add booking entity

---
+++ server/src/main/java/com/smartcampus/server/entity/Booking.java
@@ -0,0 +1,58 @@
+package com.smartcampus.server.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.smartcampus.server.model.Resource;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    private LocalDate date;
+    private String purpose;
+    private String status;
+    private int attendees;
+    private String rejectReason;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    // RELATIONSHIP
+    @ManyToOne
+    @JoinColumn(name = "resourceID")
+    private Resource resource;
+
+    public Booking() {}
+
+    public Long getBookingId() { return bookingId; }
+    public void setBookingId(Long bookingId) { this.bookingId = bookingId;}
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public String getPurpose() {return purpose;}
+    public void setPurpose(String purpose) {this.purpose = purpose;}
+
+    public String getStatus() {return status;}
+    public void setStatus(String status) {this.status = status;}
+
+    public int getAttendees() {return attendees;}
+    public void setAttendees(int attendees) {this.attendees = attendees;}
+
+    public String getRejectReason() {return rejectReason;}
+    public void setRejectReason(String rejectReason) {this.rejectReason = rejectReason;}
+
+    public LocalTime getStartTime() { return startTime; }
+    public void setStartTime(LocalTime startTime) { this.startTime = startTime; }
+
+    public LocalTime getEndTime() { return endTime; }
+    public void setEndTime(LocalTime endTime) { this.endTime = endTime; }
+
+    public Resource getResource() { return resource; }
+    public void setResource(Resource resource) { this.resource = resource; }
+}
