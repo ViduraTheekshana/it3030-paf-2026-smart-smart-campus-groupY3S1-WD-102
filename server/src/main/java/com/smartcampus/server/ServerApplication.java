df99c15ae85a9ed441a6de65195afa5430945eb5
ViduraTheekshana <viduratheekshana2003@gmail.com>
2026-04-20 01:04:50 +0530

    chore(config): clean up firebase config and application properties, Implemented proper ENV

--- server/src/main/java/com/smartcampus/server/ServerApplication.java
+++ server/src/main/java/com/smartcampus/server/ServerApplication.java
@@ -3,10 +3,15 @@ package com.smartcampus.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
