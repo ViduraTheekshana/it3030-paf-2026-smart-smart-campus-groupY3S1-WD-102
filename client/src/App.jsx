552581b5b369890907375d15db72f6435c88808e
Isurini Navodya <140940021+Isurini@users.noreply.github.com>
2026-04-21 00:20:20 +0530

    "fix(client): add missing dependencies react-icons and recharts"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,15 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-import { useState } from "react";
-
-import { Header } from "./components/layout/Header";
-import { Layout } from "./components/layout/Layout";
-import { ProtectedRoute } from "./components/common/ProtectedRoute";
-
-import { NotificationProvider } from "./context/NotificationContext";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { NotificationProvider } from "./context/NotificationContext";
+import ProtectedRoute from "./ProtectedRoute";
 
-import { Toaster } from "sonner";
-
-import { Login } from "./pages/Login";
-import  Register from "./pages/auth/Register";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-import  Profile from "./pages/Profile";
 import ResourcePage from "./pages/ResourcePage";
-import  BookingPage  from "./pages/BookingPage";
+import { Header } from "./components/layout/Header";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Profile from "./pages/Profile";
+import BookingPage from "./pages/BookingPage";
 import AdminBookingManagement from "./pages/AdminBookingManagement";
 import  UserDashboard  from "./pages/UserDashboard";
 import { IncidentsList } from "./pages/incidents/IncidentsList";
@@ -40,101 +34,78 @@ const Dashboard = () => (
   </div>
 );
 
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
 function App() {
   return (
       <AuthProvider>
         <NotificationProvider>
-          <Toaster position="top-right" richColors />
-
           <Routes>
-            {/* Public */}
+            {/* Public routes without header */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
 
-            {/* Protected user routes */}
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/resources"
-              element={
-                <ProtectedRoute>
-                  <ResourcePage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/bookings"
-              element={
-                <ProtectedRoute>
-                  <BookingPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Incident routes */}
-            <Route
-              path="/incidents"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <IncidentsList />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/incidents/new"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <CreateIncident />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/incidents/:id"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <IncidentDetail />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Admin routes */}
-            <Route
-              path="/admin/userdashboard"
-              element={
-                <ProtectedRoute roles={["ROLE_ADMIN"]}>
-                  <UserDashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin/dashboard"
-              element={
-                <ProtectedRoute roles={["ROLE_ADMIN"]}>
-                  <AdminBookingManagement />
-                </ProtectedRoute>
-              }
-            />
+            {/* Routes with header */}
+            <Route element={<MainLayout />}>
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/resources"
+                element={
+                  <ProtectedRoute>
+                    <ResourcePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/bookings"
+                element={
+                  <ProtectedRoute>
+                    <BookingPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/userdashboard"
+                element={
+                  <ProtectedRoute roles={["ROLE_ADMIN"]}>
+                    <UserDashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/admin/dashboard"
+                element={
+                  <ProtectedRoute roles={["ROLE_ADMIN"]}>
+                    <AdminBookingManagement />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
 
             {/* Default */}
             <Route path="*" element={<Login />} />
