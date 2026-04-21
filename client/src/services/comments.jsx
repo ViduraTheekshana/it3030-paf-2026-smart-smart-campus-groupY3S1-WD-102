aaef05882502d3bdb22c2a5919356c533de6001b
ViduraTheekshana <viduratheekshana2003@gmail.com>
2026-04-21 21:26:50 +0530

    feat(client): add incident, comment, user and notification api services

---
+++ client/src/services/comments.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { api } from "./api";
+export const addComment = async (incidentId, content) => {
+	const response = await api.post(`/incidents/${incidentId}/comments`, {
+		content,
+	});
+	return response.data;
+};
+
+export const editComment = async (incidentId, commentId, content) => {
+	const response = await api.patch(
+		`/incidents/${incidentId}/comments/${commentId}`,
+		{
+			content,
+		},
+	);
+	return response.data;
+};
+
+export const deleteComment = async (incidentId, commentId) => {
+	const response = await api.delete(
+		`/incidents/${incidentId}/comments/${commentId}`,
+	);
+	return response.data;
+};
---
+++ client/src/services/user.jsx
@@ -0,0 +1,23 @@
+import { api } from "./api";
+
+export const getAllUsers = async () => {
+	const response = await api.get("/admin/users");
+	return response.data;
+};
+
+export const getUsersByRole = async (roleName) => {
+	const allUsers = await getAllUsers();
+	return allUsers.filter((user) => user.role === roleName);
+};
+
+export const getTechnicians = async () => {
+	return await getUsersByRole("ROLE_TECHNICIAN");
+};
+
+export const getAdmins = async () => {
+	return await getUsersByRole("ROLE_ADMIN");
+};
+
+export const getRegularUsers = async () => {
+	return await getUsersByRole("ROLE_USER");
+};
